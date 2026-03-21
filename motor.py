a8756344e2f1fcff58a7faa671a477b54184f249
jak713 <juliakaczmarek00@gmail.com>
2026-03-21 13:40:41 +0000

    Add DCMotor library & trial motor script

---
+++ motor.py
@@ -0,0 +1,32 @@
+"""
+Simple motor script.
+"""
+from machine import Pin, PWM
+from time import sleep
+from dcmotor import DCMotor
+
+frequency = 1000 # Hz
+
+Pin1 = Pin(1, Pin.OUT)
+Pin2 = Pin(2, Pin.OUT)
+enable = PWM(Pin(2), freq=frequency)
+
+dc_motor = DCMotor(Pin1, Pin2, enable)
+
+try:
+    print('Forward with speed: 50%')
+    dc_motor.forward(50)
+    sleep(5)
+    dc_motor.stop()
+    sleep(5)
+    print('Backwards with speed: 100%')
+    dc_motor.backwards(100)
+    sleep(5)
+    print('Forward with speed: 5%')
+    dc_motor.forward(5)
+    sleep(5)
+    dc_motor.stop()
+    
+except KeyboardInterrupt:
+    print('Keyboard Interrupt')
+    dc_motor.stop()
